de1d268db73833e4dc8935a341fe627ef91103d4
AnishGh1997 <anishsubramanian1997@gmail.com>
2022-03-07 18:55:37 +0530

    TC 01

---
+++ src/com/dummy/Arry.java
@@ -0,0 +1,23 @@
+package com.dummy;
+
+public class Arry {
+	public static void main(String[] args) {
+		int [] a = new int[5]; // array is used to store multiple value in single variable
+		a[0]=6;
+		a[1]=7;
+		a[2]=5;
+		a[3]=9;
+		a[4]=3;
+//		a[5]=11;
+//		a[6]=32;
+		
+		
+		int length = a.length;
+		System.out.println(length);
+		System.out.println(a[4]);
+		
+		
+		
+	}
+
+}
---
+++ src/com/dummy/PrintOneToHundred.java
@@ -0,0 +1,20 @@
+package com.dummy;
+
+public class PrintOneToHundred {    // without using number we can print 1 to hundred
+	
+	public static void main(String[] args) {
+		int one = 'a'/'a';  // same thing will be divided we get one as a answer number
+		
+		String s = "**********"; //ten times of symbols stored in this string so its length value is 10
+		
+		for (int i = one; i <=(s.length()*s.length()); i++) {
+			
+			System.out.println(i);
+			
+		}
+		
+		
+		
+	}
+
+}
